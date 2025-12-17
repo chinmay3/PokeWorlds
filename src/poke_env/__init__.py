e17aec5f1278baea920c4da168d5a75f745d7deb
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-17 10:49:13 -0800

    This is how I'm going to do it. Will have out links in here perhaps idk. Maybe a dedicated docs folder later

--- src/poke_env/__init__.py
+++ src/poke_env/__init__.py
@@ -1,4 +1,13 @@
-# Import in order we want them to appear in documentation
+"""
+TODO: DESCRIPTION
+
+Useful parts to look at:
+Core APIs:
+
+Development Objects:
+
+"""
+
 from poke_env.emulators.emulator import LowLevelActions
 from poke_env.emulators.pokemon import AVAILABLE_POKEMON_VARIANTS, get_pokemon_emulator
 from poke_env.emulators.emulator import Emulator, GameStateParser
