76d1c738c741c915c8160f6abd21abd528583375
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-18 12:26:00 -0800

    More docs

--- src/poke_worlds/__init__.py
+++ src/poke_worlds/__init__.py
@@ -16,7 +16,12 @@ The `emulators` subpackage does most of the heavy lifting in this project. It de
 Briefly skim the documentation for each of these classes to understand their roles, the fundamental structure they impose and how they interact with each other.
 
 In practice, unless you are implementing new games, you will not need to interact with these base classes directly. The `StateParser` and `StateTracker` have subclasses that implement Pokémon specific logic and provides some additional structure.
-This is what you should familiarize yourself with most deeply if you wish to use this package as a black box API and not care about the internals. Search for the pokemon submodule and look at the parsers and trackers defined there. 
+This is what you should familiarize yourself with most deeply if you wish to use this package as a black box API and not care about the internals. Search for the `pokemon` submodule and look at the parsers and trackers defined there. 
+
+### Notable Imports
+* `LowLevelActions`: Enum defining low level actions that can be performed in the emulator.
+* `AVAILABLE_POKEMON_VARIANTS`: List of available Pokémon game variants supported by the package.
+* `get_pokemon_emulator`: Factory function to get an emulator instance for a specified Pokémon game variant.
 
 """
 from poke_worlds.emulators.emulator import LowLevelActions
