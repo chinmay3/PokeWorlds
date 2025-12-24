ee08b9c927873bb847ee1eafdc3790d52e489d75
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-23 16:00:41 -0800

    plz

--- demos/vlm_agent.py
+++ demos/vlm_agent.py
@@ -76,7 +76,9 @@ Now, based on the current frame and the context, first think and reason about yo
             return_tensors="pt"
         )   
         inputs = inputs.to(self.model.device)
-        generated_ids = self.model.generate(**inputs, max_new_tokens=500, stop_strings=["</action>"], tokenizer=self.processor.tokenizer)
+        generated_ids = self.model.generate(**inputs, max_new_tokens=500, stop_strings=["</action>"], tokenizer=self.processor.tokenizer, 
+                                            do_sample=True, 
+                                            temperature=0.9)
         generated_ids_trimmed = [
             out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
         ]
