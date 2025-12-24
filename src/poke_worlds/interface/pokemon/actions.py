ea6090ee49658814aad61ff701620b7bf7a8161b
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-23 16:10:06 -0800

    Silly

--- src/poke_worlds/interface/pokemon/actions.py
+++ src/poke_worlds/interface/pokemon/actions.py
@@ -87,7 +87,7 @@ class InteractAction(HighLevelAction):
                 break
             prev_frames.append(frame)        
         if action_success == 0:
-            action_success == -1 # I guess?
+            action_success = -1 # I guess?
         return [self._state_tracker.report()], action_success # 0 means something likely happened. 1 means def happened. 
     
     def get_action_space(self):
