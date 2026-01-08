5efca766ea6d43ab527313e4bb99a23188964f55
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-01-08 02:24:00 -0800

    Fixing OCR in Report

--- src/poke_worlds/execution/pokemon/reports.py
+++ src/poke_worlds/execution/pokemon/reports.py
@@ -1,3 +1,4 @@
+from poke_worlds.utils.vlm import _ocr_merge
 from poke_worlds.execution.report import ExecutionReport
 from poke_worlds.emulation.pokemon.trackers import PokemonOCRTracker
 from typing import Dict, Any
@@ -9,6 +10,14 @@ class PokemonExecutionReport(ExecutionReport):
         # just get the OCR text info:
         if "ocr" in state_info and "transition_ocr_texts" in state_info["ocr"]:
             ocr_texts = state_info["ocr"]["transition_ocr_texts"]
-            return "OCR Info: " + "\n".join([f"{k}: {v}" for k, v in ocr_texts.items()])
+            all_ocrs = {}
+            for ocr_text_dict in ocr_texts:
+                for key in ocr_text_dict:
+                    if key not in all_ocrs:
+                        all_ocrs[key] = []
+                    all_ocrs[key].append(ocr_text_dict[key])
+            for key in all_ocrs:
+                all_ocrs[key] = _ocr_merge(all_ocrs[key])
+            return "OCR Results: " + "\n".join([f"{k}: {v}" for k, v in all_ocrs.items()])
         else:
             return ""
