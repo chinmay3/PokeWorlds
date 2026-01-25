64125c2b144327452003a3cc79c65be2049de03b
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-01-25 11:35:40 -0800

    VITAL CHANGE: Made default PokemonExecutor and PokemonEQAExecutor use only HighLevelActions and no ExecutorActions

--- src/poke_worlds/execution/pokemon/executors.py
+++ src/poke_worlds/execution/pokemon/executors.py
@@ -18,7 +18,7 @@ from poke_worlds.execution.pokemon.executor_actions import (
 from poke_worlds.emulation.pokemon.parsers import AgentState
 
 
-class SimplePokemonExecutor(SimpleExecutor):
+class PokemonExecutor(SimpleExecutor):
     REQUIRED_CONTROLLER = PokemonStateWiseController
 
     def _string_to_executor_action(self, action_str):
@@ -38,7 +38,6 @@ class SimplePokemonExecutor(SimpleExecutor):
         if last_action_hint is False:
             interaction_advice = ""
             path_blocked_advice = ""
-            locate_advice = ""
         action_success_message = ""
         if action == MoveStepsAction or action == MoveGridAction:
             if action_success == 0:
@@ -86,7 +85,7 @@ class SimplePokemonExecutor(SimpleExecutor):
         return action_success_message
 
 
-class PokemonExecutor(SimpleExecutor):
+class ExecutorActionsPokemonExecutor(SimpleExecutor):
     REQUIRED_CONTROLLER = PokemonStateWiseController
     REQUIRED_ENVIRONMENT = PokemonEnvironment
     EXECUTOR_ACTIONS = [PokemonLocateAction, CheckInteractionAction]
@@ -235,123 +234,7 @@ class PokemonExecutor(SimpleExecutor):
             )
         return action_str
 
-    def get_action_message(
-        self,
-        *,
-        action,
-        action_kwargs: dict,
-        action_success: int,
-        action_return: dict,
-        last_action_hint: bool = False,
-    ):
-        interaction_advice = f"If you want to try to interact with it, use interact() or checkinteraction() to get an estimate on whether or not you can interact with it."
-        path_blocked_advice = f"If you are trying to go somewhere, then this direction is blocked. Try moving around it or going a different way."
-        locate_advice = f"You may need to move around, or trust your visual intuition from the screen."
-        if last_action_hint is False:
-            interaction_advice = ""
-            path_blocked_advice = ""
-            locate_advice = ""
-        action_success_message = ""
-        if action == MoveStepsAction or action == MoveGridAction:
-            if action_success == 0:
-                if action_return["rotated"] == True:
-                    action_success_message = f"You did not actually move, but rotated to face the direction you wanted to move in. This means there is now either an obstacle or object / NPC in front of you. {interaction_advice} {path_blocked_advice}"
-                else:
-                    action_success_message = "You moved the exact number of steps you wanted to move. You are now facing the target location."
-            if action_success == 1:
-                action_success_message = f"You moved until you hit a wall, object, NPC or obstacle. {interaction_advice} {path_blocked_advice}"
-            if action_success == -1:
-                action_success_message = f"You could not move in that direction at all. There is an obstacle in the way. {interaction_advice} {path_blocked_advice}"
-            if action_success == 2:
-                action_success_message = "You moved, but before you could finish your steps, you were interupted by a battle, dialogue or cutscene."
-        elif action == CheckInteractionAction:
-            if action_success == -1:
-                action_success_message = "There is nothing to interact from your current position, consider using the locate action to get the exact coordinates before moving to that spot and trying again."
-            else:
-                found_directions = {}
-                for direction in ["up", "down", "left", "right"]:
-                    if action_return[direction][1] == True:
-                        found_directions[direction] = (
-                            f"In the direction {direction}, there is {action_return[direction][0]}. To interact with this, first use the action `move({direction} 1)` and then use `interact()` again."
-                        )
-                action_success_message = f"There are things you may be able to interact with in certain directions from your current position.\n"
-                for found_direction, found_desc in found_directions.items():
-                    action_success_message += f"\t{found_desc}\n"
-        elif action == PokemonLocateAction:
-            if action_success == -1:
-                action_success_message = (
-                    f"Could not locate the target on screen. {locate_advice}"
-                )
-            else:
-                potential_cells_str = action_return["potential_cells_str"]
-                definitive_cells_str = action_return["definitive_cells_str"]
-                action_success_message = f"Located the target on screen. Potential cells: {potential_cells_str}. Definitive cells: {definitive_cells_str}. Trust that these are accurate. You can now either move() towards one of these coordinates to interact() or do something else."
-        elif action == InteractAction:
-            if action_success == -1:
-                action_success_message = "There was nothing to interact with in front of you. Make sure you are facing an object or character and are right next to it. Move into an object or NPC to face them."
-            if action_success == 1:
-                action_success_message = "Your interaction led to something."
-        elif action == PassDialogueAction:
-            if action_success == -1:
-                action_success_message = (
-                    "There was no dialogue to pass through. Check the state"
-                )
-        elif action == MenuAction:
-            if action_success == -1:
-                action_success_message = "The menu action could not be performed. Check if you are in the menu and that the action is valid."
-        elif action == BattleMenuAction:
-            if action_success == -1:
-                action_success_message = "The battle menu action could not be performed. Check if you are in a battle and that the action is valid."
-            elif action_success == 1:
-                action_success = "Tried to run, but the wild pokemon was too fast and you could not escape."
-            elif action_success == 2:
-                action_success = (
-                    "Tried to run, but you cannot run from trainer battles."
-                )
-        elif action == PickAttackAction:
-            if action_success == -1:
-                action_success_message = "Could not pick that attack. Check if you are in the attack menu and that the attack index is valid."
-            if action_success == 1:
-                action_success_message = (
-                    "Insufficient pp for that move. Pick another move."
-                )
-        else:
-            action_success_message = f"UNHANDLED CASE: action={action}, args={action_kwargs}, action_success={action_success}"
-        return action_success_message
 
-
-class EQAPokemonExecutor(EQAExecutor):
+class EQAPokemonExecutor(EQAExecutor, PokemonExecutor):
     REQUIRED_CONTROLLER = PokemonStateWiseController
-    REQUIRED_ENVIRONMENT = PokemonEnvironment
-    EXECUTOR_ACTIONS = [PokemonLocateAction, CheckInteractionAction]
-
-    def get_action_str(
-        self, *, allowed_actions_str, prev_action_strings=[], system_prompt=""
-    ):
-        return PokemonExecutor.get_action_str(
-            self,
-            allowed_actions_str=allowed_actions_str,
-            prev_action_strings=prev_action_strings,
-            system_prompt=system_prompt,
-        )
-
-    def _string_to_executor_action(self, action_str):
-        return PokemonExecutor._string_to_executor_action(self, action_str)
-
-    def get_action_message(
-        self,
-        *,
-        action,
-        action_kwargs,
-        action_success,
-        action_return,
-        last_action_hint=False,
-    ):
-        return PokemonExecutor.get_action_message(
-            self,
-            action=action,
-            action_kwargs=action_kwargs,
-            action_success=action_success,
-            action_return=action_return,
-            last_action_hint=last_action_hint,
-        )
+    # TODO: Confirm that the inheritance order here is correct
